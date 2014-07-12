150c66b2fbe46462863ce4d2ed8e85fa709c471b
Bill Linux <bill.linux@laposte.net>
2014-07-11 21:51:26 -0300

    Created 'netrc' & modified 'uninstall.sh'

--- uninstall.sh
+++ uninstall.sh
@@ -11,8 +11,30 @@ die() {
     exit 1
 }
 
+unlink $HOME/.bash_profile
+unlink $HOME/.bashrc
+unlink $HOME/.profile
+unlink $HOME/.shellactivities
+unlink $HOME/.shellaliases
+unlink $HOME/.shellpaths
+unlink $HOME/.shellvars
+unlink $HOME/.shellcolors
+unlink $HOME/.zlogout
+unlink $HOME/.zprofile
+unlink $HOME/.zshenv
+unlink $HOME/.zshrc
+
+unlink $HOME/.vimrc.bundles
+unlink $HOME/.vimrc.before
+
+
+unlink $HOME/.gemrc
+unlink $HOME/.rvmc
+unlink $HOME/.netrc
+unlink $HOME/.gitconfig
+unlink $HOME/.slate
+
 rm $HOME/.vimrc
-rm $HOME/.vimrc.bundles
 rm $HOME/.vim
 
 rm -rf $app_dir
