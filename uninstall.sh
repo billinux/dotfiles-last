ce83a6037fcd464846ef81ecea84d5200f56f110
Bill Linux <bill.linux@laposte.net>
2014-07-11 22:10:21 -0300

    modified 'uninstall.sh'

--- uninstall.sh
+++ uninstall.sh
@@ -11,34 +11,34 @@ die() {
     exit 1
 }
 
-unlink $HOME/.bash_profile
-unlink $HOME/.bashrc
-unlink $HOME/.profile
-unlink $HOME/.shellactivities
-unlink $HOME/.shellaliases
-unlink $HOME/.shellpaths
-unlink $HOME/.shellvars
-unlink $HOME/.shellcolors
-unlink $HOME/.zlogout
-unlink $HOME/.zprofile
-unlink $HOME/.zshenv
-unlink $HOME/.zshrc
+unlink $HOME/.bash_profile > /dev/null 2>&1
+unlink $HOME/.bashrc > /dev/null 2>&1
+unlink $HOME/.profile > /dev/null 2>&1
+unlink $HOME/.shellactivities > /dev/null 2>&1
+unlink $HOME/.shellaliases > /dev/null 2>&1
+unlink $HOME/.shellpaths > /dev/null 2>&1
+unlink $HOME/.shellvars > /dev/null 2>&1
+unlink $HOME/.shellcolors > /dev/null 2>&1
+unlink $HOME/.zlogout > /dev/null 2>&1
+unlink $HOME/.zprofile > /dev/null 2>&1
+unlink $HOME/.zshenv > /dev/null 2>&1
+unlink $HOME/.zshrc > /dev/null 2>&1
 
-unlink $HOME/.vimrc.bundles
-unlink $HOME/.vimrc.before
+unlink $HOME/.vimrc.bundles > /dev/null 2>&1
+unlink $HOME/.vimrc.before > /dev/null 2>&1
 
+unlink $HOME/.gemrc > /dev/null 2>&1
+unlink $HOME/.rvmc > /dev/null 2>&1
+unlink $HOME/.netrc > /dev/null 2>&1
+unlink $HOME/.gitconfig > /dev/null 2>&1
+unlink $HOME/.slate > /dev/null 2>&1
 
-unlink $HOME/.gemrc
-unlink $HOME/.rvmc
-unlink $HOME/.netrc
-unlink $HOME/.gitconfig
-unlink $HOME/.slate
+rm $HOME/.vimrc > /dev/null 2>&1
+rm $HOME/.vim > /dev/null 2>&1
+rm $HOME/.vimrc.local > /dev/null 2>&1
 
-rm $HOME/.vimrc
-rm $HOME/.vim
-
-rm -rf $app_dir
-rm -rf ~/.vimbackup
-rm -rf ~/.vimswap
-rm -rf ~/.vimundo
-rm -rf ~/.vimviews
+rm -rf $app_dir > /dev/null 2>&1
+rm -rf ~/.vimbackup > /dev/null 2>&1
+rm -rf ~/.vimswap > /dev/null 2>&1
+rm -rf ~/.vimundo > /dev/null 2>&1
+rm -rf ~/.vimviews > /dev/null 2>&1
